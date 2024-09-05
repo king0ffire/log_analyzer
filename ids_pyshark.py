ac2e2c1df024fe6c2f9bf93442464c7708618d1c
mengqingxin <mengqingxin@baicells.com>
2024-09-05 16:10:44 +0800

    linux上的tshark版本不一样，通过一个config来指定是哪种时间戳

--- ids_pyshark.py
+++ ids_pyshark.py
@@ -1,5 +1,7 @@
 import asyncio
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+import configparser
+import datetime
 import gzip
 import logging
 import os
@@ -9,6 +11,8 @@ import pyshark
 from os.path import basename
 
 logger=logging.getLogger(__name__)
+config=configparser.ConfigParser()
+config.read("config.ini")
 def pcapInfoToListBy2Filters(filename, filter1, filter2, eventloop=None):
     csvlist=[]
     display_filter=filter1+"||"+filter2
@@ -21,7 +25,8 @@ def pcapInfoToListBy2Filters(filename, filter1, filter2, eventloop=None):
                     #capdetail.load_packets()
                     #capsummary.load_packets()
                     for packetsummary, packetdetail in zip(capsummary, capdetail): 
-                            csvlist.append([basename(filename),packetsummary.no,packetdetail.sniff_timestamp,
+                            date=packetdetail.sniff_timestamp if config["python"]["timestamptodate"]=="False" else datetime.datetime.fromtimestamp(float(packetdetail.sniff_timestamp))
+                            csvlist.append([basename(filename),packetsummary.no,date,
                                             packetsummary.source,packetsummary.destination,
                                             packetsummary.protocol,packetsummary.info,
                                             packetdetail.s1ap.get_field(field1.upper()),
